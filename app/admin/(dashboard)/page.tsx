1fbfa4a8fe0e594569a40c62c2d5dcabb0e67cb0
Nguyen Duong <nduongg04@gmail.com>
2024-10-06 23:55:12 +0700

    Fix conflicts web

--- app/admin/(dashboard)/page.tsx
+++ app/admin/(dashboard)/page.tsx
@@ -4,16 +4,16 @@ import SearchBar from "@/components/admin/SearchBar";
 
 const Dashboard = () => {
   return (
-	<div className="flex flex-col gap-10 flex-1 p-5">
-		<div className="flex justify-between gap-10  font-barlow h-10">
-			<SearchBar />
-			<div className="h-full w-px bg-bright-gray" />
-			<Profile />
-		</div>
-		<div className="flex justify-between">
-			<Badge  />
-		</div>
-	</div>
-  )
+    <div className="flex flex-1 flex-col gap-10 p-5">
+      <div className="flex h-10 justify-between gap-10 font-barlow">
+        <SearchBar />
+        <div className="h-full w-px bg-bright-gray" />
+        <Profile />
+      </div>
+      <div className="flex justify-between">
+        <Badge />
+      </div>
+    </div>
+  );
 };
 export default Dashboard;
