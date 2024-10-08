261f8760c007c5b8adf90721b60988184ef8bb59
Tran Thanh Dat <neban0444@gmail.com>
2024-10-08 16:49:32 +0700

    fix table UI

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,7 +4,9 @@ const AdminLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="flex flex-row w-screen bg-[#F3F2F7]">
 			<NavBar />
+			<div className="w-[100%]">
 			{children}
+			</div>
 		</div>
 	);
 };
