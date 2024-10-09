b783d6c8711bf36f457d14be0bedfb0622602f0f
Tran Thanh Dat <neban0444@gmail.com>
2024-10-10 00:04:24 +0700

    customer, employee, membership UI

---
+++ app/admin/customer/data-table.tsx
@@ -0,0 +1,216 @@
+"use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    ColumnFiltersState,
+    getFilteredRowModel,
+    SortingState,
+    useReactTable,
+} from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { TriangleAlert } from "lucide-react"
+import * as React from "react"
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+    columns,
+    data,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
+  const [rowSelection, setRowSelection] = React.useState({})
+  // Custom filter function for date range
+  
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      rowSelection,
+    }
+  });
+  
+  return (
+    <div>
+      <div className="flex items-center justify-between py-4">
+        <Input
+          placeholder="Search by name"
+          value={(table.getColumn("customerName")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("customerName")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm bg-white rounded-md"
+        />
+        <div className="flex justify-end">         
+          <Button variant="secondary" className="mx-4">
+            Download all
+          </Button>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive">Delete customer</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader className="flex flex-col items-center">
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <TriangleAlert className="text-[#ff0000] size-20" />
+                Are you sure you want to delete the selected customers?
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction className="bg-green-600 hover:bg-green-500">
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="green" className="mx-4">Add customer</Button>
+            </DialogTrigger>
+            <DialogContent >
+              <DialogHeader>
+                <DialogTitle>Add new customer</DialogTitle>
+                <DialogDescription>
+                  Add new customer here. Click save when you're done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4 w-[100%]">
+                <table className="border-spacing-2 border-separate">
+                  <tr >
+                    <td >Full name</td>
+                    <td ><Input placeholder="Fullname" /></td>
+                  </tr>
+                  <tr >
+                    <td >Phone Number</td>
+                    <td ><Input placeholder="Phone Number"/></td>
+                  </tr>              
+                </table>            
+              </div>
+              <DialogFooter>
+                <Button type="submit" className="bg-green-600 hover:bg-green-500">Save changes</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>          
+        </div>
+      </div>
+
+      <div className="rounded-md border p-4 bg-white">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="p-2">
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+
+      <div className="flex">
+        <div className="flex-1 text-sm text-muted-foreground mt-2">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
---
+++ app/admin/employee/data-table.tsx
@@ -0,0 +1,229 @@
+"use client";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    ColumnFiltersState,
+    getFilteredRowModel,
+    SortingState,
+    useReactTable,
+} from "@tanstack/react-table";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { TriangleAlert } from "lucide-react"
+import * as React from "react"
+interface DataTableProps<TData, TValue> {
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+    columns,
+    data,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = React.useState<SortingState>([])
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
+  const [rowSelection, setRowSelection] = React.useState({})
+  // Custom filter function for date range
+  
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      rowSelection,
+    }
+  });
+  
+  return (
+    <div>
+      <div className="flex items-center justify-between py-4">
+        <Input
+          placeholder="Search by name"
+          value={(table.getColumn("staffName")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("staffName")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm bg-white rounded-md"
+        />
+        <div className="flex justify-end">         
+          <Button variant="secondary" className="mx-4">
+            Download all
+          </Button>
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive">Delete staff</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader className="flex flex-col items-center">
+                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                <TriangleAlert className="text-[#ff0000] size-20" />
+                Are you sure you want to delete the selected staff?
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction className="bg-green-600 hover:bg-green-500">
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button variant="green" className="mx-4">Add staff</Button>
+            </DialogTrigger>
+            <DialogContent >
+              <DialogHeader>
+                <DialogTitle>Add new staff</DialogTitle>
+                <DialogDescription>
+                  Add new staff here. Click save when you're done.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-4 py-4 w-[100%]">
+                <table className="border-spacing-2 border-separate">
+                  <tr>
+                    <td >Role</td>
+                    <td >
+                      <select className="border rounded-md p-1">
+                        <option value="Admin">Admin</option>
+                        <option value="Staff">Staff</option>
+                      </select>
+                    </td>
+                  </tr>
+                  <tr >
+                    <td >Full name</td>
+                    <td ><Input placeholder="Full name" /></td>
+                  </tr>
+                  <tr >
+                    <td >Account</td>
+                    <td ><Input placeholder="Username" /></td>
+                  </tr>
+                  <tr>
+                    <td >Password</td>
+                    <td ><Input type="password" placeholder="Password" /></td>
+                  </tr>
+                </table>            
+              </div>
+              <DialogFooter>
+                <Button type="submit" className="bg-green-600 hover:bg-green-500">Save changes</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>          
+        </div>
+      </div>
+
+      <div className="rounded-md border p-4 bg-white">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id} className="p-2">
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+
+      <div className="flex">
+        <div className="flex-1 text-sm text-muted-foreground mt-2">
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
