9f8c5150832c5b74eab402f0b498a60b911473e8
Tran Thanh Dat <neban0444@gmail.com>
2024-10-29 16:12:32 +0700

    fetching api

--- app/admin/bill/columns.tsx
+++ app/admin/bill/columns.tsx
@@ -1,5 +1,5 @@
-"use client"
-
+"use client";
+import * as React from "react"
 import { ColumnDef } from "@tanstack/react-table"
 import { Pencil, ArrowUpDown } from "lucide-react"
 import { Fragment } from "react"
@@ -12,22 +12,14 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-
+import { toast } from "sonner"
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
 import { FilterFn } from "@tanstack/react-table";
+
+import BillDialog from "./bill-dialog";
+
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type BillInfo = {
@@ -35,18 +27,20 @@ export type BillInfo = {
   productID: string
   productName: string
   productCount: number
-  totalPrice: number
+  totalPriceDtail: number
   productPrice: number
 }
 export type Bill = {
   id: string
-  customerId: string
-  voucherId: string
+  customer: string
+  voucherValue: number
   staffId: string
-  payTypeId: string
-  status: "Waiting" | "Paid" 
+  staff: string
+  payType: string
+  status: "Pending" | "Successful" 
   totalPrice: number
   createdAt: Date
+  dateString: string
   billInfo: BillInfo[]
 }
 export const dateRangeFilter: FilterFn<any> = (row, columnId, filterValue) => {
@@ -64,15 +58,20 @@ export const dateRangeFilter: FilterFn<any> = (row, columnId, filterValue) => {
   }
   return true; // No filter if both start and end are undefined
 };
-export const columns: ColumnDef<Bill>[] = [
+export const columns: (onUpdate: (billId: string, newStatus: "Pending" | "Successful") => void) => ColumnDef<Bill>[] = (onUpdate) =>[
   {
+    id: "id",
     accessorKey: "id",
     header: "ID",
   },
   {
+    id: "createdAt",
     accessorKey: "createdAt",
-    header: "Created At",
-    filterFn: dateRangeFilter,
+    filterFn: dateRangeFilter
+  },
+  {
+    accessorKey: "dateString",
+    header: "Created At"
   },
   {
     accessorKey: "totalPrice",
@@ -101,108 +100,29 @@ export const columns: ColumnDef<Bill>[] = [
     accessorKey: "status",
     header: "Status",
     cell: ({ row }) => {
-      return row.getValue("status")==="Waiting"? <Label className="text-red-500">Waiting</Label> : <Label className="text-green-500">Paid</Label>
+      return row.getValue("status")==="Pending"? <Label className="text-red-500">Pending</Label> : <Label className="text-green-500">Successful</Label>
     }
   },
   {
     id: "actions",
       cell: ({ row }) => {
         const bill = row.original
+        const [billStatus, setBillStatus] = React.useState(bill.status);
+        const handleUpdate = (status:"Pending"|"Successful") => {
+          setBillStatus(status);
+          row.original.status = status;
+          onUpdate(bill.id, status);
+        };
         return (
           <Dialog>
             <DialogTrigger asChild>
               <Pencil className="size-4 cursor-pointer"/>
             </DialogTrigger>
-            <DialogContent className="sm:max-w-[925px]">
-              <DialogHeader>
-                <DialogTitle>Edit bill status #{bill.id}</DialogTitle>
-                <DialogDescription>
-                  Make changes to bill status here. Click save when you're done.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="grid gap-4 py-4 w-[100%]">
-                <div className="flex justify-between">
-                    <Label 
-                      >Staff: <span className="font-normal">{bill.staffId}</span></Label>            
-                    <Label 
-                      >Customer: <span className="font-normal">{bill.customerId}</span></Label>
-                    <div className="flex">
-                      <Label className="mx-4">Status</Label>
-                      <RadioGroup defaultValue={bill.status}>
-                        <div className="flex items-center space-x-2 text-red-500">
-                          <RadioGroupItem value="Waiting" id="r1" />
-                          <Label htmlFor="r1">Waiting</Label>
-                        </div>
-                        <div className="flex items-center space-x-2 text-green-400">
-                          <RadioGroupItem value="Paid" id="r2" />
-                          <Label htmlFor="r2">Paid</Label>
-                        </div>                   
-                      </RadioGroup>              
-                    </div>
-                </div>
-                <Label>Product List</Label>
-                <div className="overflow-y-auto max-h-48">
-                    <Table>
-                    <TableHeader>
-                      <TableRow>
-                        <TableHead>No</TableHead>
-                        <TableHead>Product</TableHead>
-                        <TableHead>Quantity</TableHead>
-                        <TableHead>Price</TableHead>
-                        <TableHead >Total Price</TableHead>
-                      </TableRow>
-                    </TableHeader>
-                    <TableBody >
-                      {
-                        bill.billInfo.map((billInfo, index) => (
-                          <Fragment key={billInfo.productID}>
-                              <TableRow>
-                                <TableCell className="text-left">{index + 1}</TableCell>
-                                <TableCell>{billInfo.productName}</TableCell>
-                                <TableCell>{billInfo.productCount}</TableCell>
-                                <TableCell>{billInfo.productPrice}</TableCell>
-                                <TableCell>{billInfo.totalPrice}</TableCell>
-                              </TableRow>
-                            </Fragment>
-                          ))
-                        }
-                    </TableBody>
-                  </Table>
-                </div>
-                <div className="flex justify-between">
-                  <Label>Payment Type: <span className="font-normal">{bill.payTypeId}</span></Label>       
-                  <Label>Created At: <span className="font-normal">{bill.createdAt.toUTCString()}</span></Label>   
-                </div>
-                <Label htmlFor="totalPrice" className="text-red-600 text-right">Sub Total: <span>{bill.totalPrice}</span></Label>
-              </div>
-              <DialogFooter>
-                <Button type="submit" className="bg-green-600 hover:bg-green-500">Save changes</Button>
-              </DialogFooter>
-            </DialogContent>
+            <BillDialog bill={bill} onUpdate={handleUpdate}>
+
+            </BillDialog>
           </Dialog>
         )
     }
-  },
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
+  }
 ]
