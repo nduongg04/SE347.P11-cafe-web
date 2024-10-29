d0da31467e6eab7eaf6062dc17167d70ada1c11d
DuongWuangDat <duongquangdat172004@gmail.com>
2024-10-29 17:41:58 +0700

    fix cfl

--- constants/index.ts
+++ constants/index.ts
@@ -5,4 +5,5 @@ export const staff = {
     avatar: "https://github.com/shadcn.png",
 };
 export const url = "https://coffeemanagementapi.azurewebsites.net/api/v1";
-export const token = "";
+export const token =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMSIsInR5cCI6IkFjY2VzcyIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6WyJBZG1pbiIsIlVzZXIiXSwiZXhwIjoxNzMwMjAxNzc1LCJpc3MiOiJodHRwczovL2NvZmZlZW1hbmFnZW1lbnRhcGkuYXp1cmV3ZWJzaXRlcy5uZXQvIiwiYXVkIjoiaHR0cHM6Ly9jb2ZmZWVtYW5hZ2VtZW50YXBpLmF6dXJld2Vic2l0ZXMubmV0LyJ9.eURW40Htb9xfoxKUTvZVPwt_LPCePOSIyHYShqm43EE";
