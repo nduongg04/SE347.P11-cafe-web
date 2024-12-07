5c35422503639be393b1aba9ff577f9e56456fb7
Tran Thanh Dat <neban0444@gmail.com>
2024-12-07 19:22:06 +0700

    update env file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        BASE_URL: "https://coffeemanagementapi.azurewebsites.net/api/v1",
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.pikbest.com",
+      },
+      {
+        protocol: "https",
+        hostname: "bizweb.dktcdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  env: {
+    BASE_URL: "https://coffeemanagementapi.azurewebsites.net/api/v1",
+  },
 };
 
 export default nextConfig;
