6e312fa6acdef56f68d94e76835af9f7ef03c04e
Nguyen Duong <haminhduchaminhduc@gmail.com>
2024-12-14 01:49:20 +0700

    update table order and soldout

--- app/employee/(order)/menuOrder.tsx
+++ app/employee/(order)/menuOrder.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import { Check, ChevronsUpDown, Type } from "lucide-react"
+import { Ban, Check, ChevronsUpDown, Type } from "lucide-react"
 import {
     Command,
     CommandEmpty,
@@ -17,10 +17,12 @@ import {
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover"
-import { getProductData, getCategoryData } from './fetchingData'
+import { getProductData, getCategoryData, updateProductSoldOut } from './fetchingData'
 
 import { ScrollArea } from '@/components/ui/scroll-area'
 import Loading from 'react-loading'
+import { CheckCheck } from 'lucide-react'
+import { toast } from 'sonner'
 
 export type Product = {
     ProductId: number,
@@ -43,28 +45,41 @@ export type PrdBill = {
 }
 type Props = {
     listPrdBill: PrdBill[]
-    setListPrdBill: React.Dispatch<React.SetStateAction<PrdBill[]>>
+    setListPrdBill: React.Dispatch<React.SetStateAction<PrdBill[]>>,
+    productsData: Product[],
+    categoriesData: Category[],
+    loading: boolean,
+    soldOut: (id:number,isSoldOut:boolean)=>void
 }
-export default function MenuOrder({ listPrdBill, setListPrdBill }: Props) {
+export default function MenuOrder({ listPrdBill, setListPrdBill, productsData, categoriesData, loading,soldOut }: Props) {
     const [searchPrd, setSearchPrd] = React.useState('');
-    const [categories, setCategories] = React.useState<Category[]>([])
-    const [products, setProducts] = React.useState<Product[]>([])
     const [open, setOpen] = React.useState(false)
-    const [typeCate, setTypeCate] = React.useState("")
-    const [listPrd, setListPrd] = React.useState<Product[]>(products)
-    const [loading,setLoading]= useState(false)
-    useEffect(() => {
-        async function fetchData() {
-            setLoading(true)
-            const categories = await getCategoryData();
-            const products = await getProductData();
-            setCategories(categories);
-            setProducts(products);
-            setListPrd(products);
-            setLoading(false)
+    const [typeCate, setTypeCate] = React.useState("All")
+    const [categories, setCategories] = React.useState<Category[]>(categoriesData)
+    const [products, setProducts] = React.useState<Product[]>(productsData)
+    const [listPrd, setListPrd] = React.useState<Product[]>(productsData)
+    const [rightClick, setRightClick] = React.useState<number>(-1);
+    const setCheckRightClick = (id:number)=>{
+        if(rightClick == id){
+            setRightClick(-1)
+        }else{
+            setRightClick(id)
         }
-        fetchData();
-    }, [])
+    }
+    const updateSoldOut = async (id:number,isSoldOut:boolean)=>{
+        const res = await updateProductSoldOut(id,!isSoldOut)
+        if(res){  
+            soldOut(id,!isSoldOut)
+            toast.success("Update product sold out successfully")
+        }
+        setRightClick(-1)
+    }
+
+    useEffect(()=>{
+        setCategories(categoriesData)
+        setProducts(productsData)
+        setListPrd(productsData)
+    },[productsData,categoriesData])
     const formatted = (amount: number) => new Intl.NumberFormat("en-US", {
         style: "currency",
         currency: "VND",
@@ -102,137 +117,176 @@ export default function MenuOrder({ listPrdBill, setListPrdBill }: Props) {
 
         }
     }
-    const handleSearchPrd = (name: string) => {
+    
+    const filterPrd = (name:string,type:string)=>{
         setSearchPrd(name)
-        if (name == "" || !name) {
-            setListPrd(products);
-            return;
-        } else {
-            const filteredProducts = products.filter((prd) =>
-                prd.ProductName.toLowerCase().includes(name.toLowerCase()) // Tìm kiếm không phân biệt chữ hoa chữ thường
-            );
-            setListPrd(filteredProducts); // Cập nhật danh sách với sản phẩm đã lọc
+        setTypeCate(type)
+        let list = products;
+        if(name!=""){
+            list = list.filter((prd) => prd.ProductName.toLowerCase().includes(name.toLowerCase()))
         }
-
-    }
-    const handleChooseCategory = (id: string) => {
-        if (!id || id === "" || id === 'All') {
-            setListPrd(products);
-            return;
-        } else {
-            const filteredProducts = products.filter((prd) =>
-                String(prd.CategoryName) == id
-            );
-            setListPrd(filteredProducts); // Cập nhật danh sách với sản phẩm đã lọc
+        if(type!="All"){
+            list = list.filter((prd) => String(prd.CategoryName) == type)
         }
+        setListPrd(list)
     }
 
     return (
-        <div className='bg-white px-2 py-2 mr-2 h-[98%] mt-2 rounded-lg shadow-sm'>
-            <div className='flex justify-between px-3 mt-2'>
-                <Input
-                    placeholder="Search by name"
-                    value={searchPrd}
-                    onChange={(a) => handleSearchPrd(a.target.value)}
-                    className="max-w-xs bg-white rounded-md " />
-                <Popover open={open} onOpenChange={setOpen}>
-                    <PopoverTrigger asChild>
-                        <Button
-                            variant="outline"
-                            role="combobox"
-                            aria-expanded={open}
-                            className="w-[180px] justify-between"
-                        >
-                            {typeCate
-                                ? categories.find((category) => String(category.CategoryName) === typeCate)?.CategoryName
-                                : "All"}
-                            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                        </Button>
-                    </PopoverTrigger>
-                    <PopoverContent className="w-[200px] p-0">
-                        <Command>
-                            <CommandInput placeholder="Search category..." />
-                            <CommandList>
-                                <CommandEmpty>No category found.</CommandEmpty>
-                                <CommandGroup>
-                                    {categories.map((category) => (
-                                        <CommandItem
-                                            key={category.CategoryName}
-                                            value={String(category.CategoryName)}
-                                            onSelect={(currentValue) => {
-                                                if (currentValue === typeCate) {
-                                                    handleChooseCategory("All")
-                                                }
-                                                else {
-                                                    handleChooseCategory(currentValue)
-                                                }
-                                                setTypeCate(currentValue === typeCate ? "" : currentValue)
-                                                setOpen(false)
-
-
-                                            }}
-                                        >
-                                            <Check
-                                                className={cn(
-                                                    "mr-2 h-4 w-4",
-                                                    typeCate === String(category.CategoryName) ? "opacity-100" : "opacity-0"
-                                                )}
-                                            />
-                                            {category.CategoryName}
-                                        </CommandItem>
-                                    ))}
-                                </CommandGroup>
-                            </CommandList>
-                        </Command>
-                    </PopoverContent>
-                </Popover>
-            </div>
-            {loading?
-            <div className='flex justify-center items-center h-[660px]'>
-            <Loading
-             type="spin"
-             color="gray"
-             height={30}
-             width={30}
-             className="ml-5"
-           />
-            </div>
-            :
-            <ScrollArea className="mt-3 h-[645px] pb-1">
-                <div className="grid grid-cols-3 grid-rows-3 gap-4 px-4 py-2 h-full">
-                    {listPrd.map((prd) => (
-                        prd.IsSoldOut ?
-                            (<div
-                                key={prd.ProductId}
-                                className="border rounded-lg p-3 px-1 text-center shadow-md flex flex-col justify-between relative"
-                            >
-                                <div className="absolute inset-0 border rounded-lg bg-gray-500 bg-opacity-70 flex items-center justify-center">
-                                    <span className=" text-lg font-semibold text-red-600">Sold out</span>
-                                </div>
-                                <img src={prd.Image} alt="Product Image" className="mb-2 w-full h-32 object-contain rounded-md" />
-                                <div>
-                                    <h2 className="text-sm font-semibold h-9 flex items-center justify-center text-ellipsis overflow-y-hidden">{prd.ProductName}</h2>
-                                    <p className="text-gray-700">{formatted(prd.Price)}</p>
-                                </div>
-                            </div>)
-                            :
-                            (<div
-                                key={prd.ProductId}
-                                className="border rounded-lg p-3 px-1 text-center shadow-md flex flex-col justify-between cursor-pointer hover:bg-gray-100 active:scale-95 active:shadow-lg transition duration-150 ease-in-out"
-                                onClick={() => clickPrd(prd)}
-                            >
-                                <img src={prd.Image} alt="Product Image" className="mb-2 w-full h-32 object-contain rounded-md" />
-                                <div>
-                                    <h2 className="text-sm font-semibold h-9 flex items-center justify-center text-ellipsis overflow-y-hidden">{prd.ProductName}</h2>
-                                    <p className="text-gray-700">{formatted(prd.Price)}</p>
-                                </div>
-                            </div>)
+      <div className="mr-2 mt-2 h-[98%] rounded-lg bg-white px-2 py-2 shadow-sm">
+        <div className="mt-2 flex justify-between px-3">
+          <Input
+            placeholder="Search by name"
+            value={searchPrd}
+            onChange={(a) => filterPrd(a.target.value, typeCate)}
+            className="max-w-xs rounded-md bg-white"
+          />
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <Button
+                variant="outline"
+                role="combobox"
+                aria-expanded={open}
+                className="w-[180px] justify-between"
+              >
+                {typeCate != "All"
+                  ? categories.find(
+                      (category) => String(category.CategoryName) === typeCate,
+                    )?.CategoryName
+                  : "All"}
+                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0">
+              <Command>
+                <CommandInput placeholder="Search category..." />
+                <CommandList>
+                  <CommandEmpty>No category found.</CommandEmpty>
+                  <CommandGroup>
+                    {categories.map((category) => (
+                      <CommandItem
+                        key={category.CategoryName}
+                        value={String(category.CategoryName)}
+                        onSelect={(currentValue) => {
+                          if (currentValue === typeCate) {
+                            filterPrd(searchPrd, currentValue);
+                          } else {
+                            filterPrd(searchPrd, currentValue);
+                          }
+                          setTypeCate(
+                            currentValue === typeCate ? "All" : currentValue,
+                          );
+                          setOpen(false);
+                        }}
+                      >
+                        <Check
+                          className={cn(
+                            "mr-2 h-4 w-4",
+                            typeCate === String(category.CategoryName)
+                              ? "opacity-100"
+                              : "opacity-0",
+                          )}
+                        />
+                        {category.CategoryName}
+                      </CommandItem>
                     ))}
-                </div>
-            </ScrollArea>
-}
-           
-
+                  </CommandGroup>
+                </CommandList>
+              </Command>
+            </PopoverContent>
+          </Popover>
         </div>
-    )
+        {loading ? (
+          <div className="mt-3 flex h-[580px] items-center justify-center">
+            <Loading
+              type="spin"
+              color="gray"
+              height={30}
+              width={30}
+              className="ml-5"
+            />
+          </div>
+        ) : (
+          <ScrollArea className="mt-3 h-[580px] pb-1">
+            <div className="grid h-full grid-cols-3 grid-rows-3 gap-4 px-4 py-2">
+              {listPrd.map((prd) =>{
+                return prd.IsSoldOut ? (
+                  <div
+                    key={prd.ProductId}
+                    onContextMenu={(e) => {
+                      e.preventDefault(); // Ngăn chặn menu ngữ cảnh mặc định
+                      setCheckRightClick(prd.ProductId);
+                      console.log(`Right click on ${prd.ProductName}`);
+                    }}
+                    className="relative flex flex-col justify-between rounded-lg border p-3 px-1 text-center shadow-md"
+                  >
+                    <div className="absolute inset-0 flex items-center justify-center rounded-lg border bg-gray-500 bg-opacity-70">
+                      {rightClick == prd.ProductId ? (
+                        <Button
+                          size="sm"
+                          variant="secondary"
+                          className="mr-2 bg-[#00B074] text-white hover:bg-[#00956A]"
+                          onClick={() => updateSoldOut(prd.ProductId,prd.IsSoldOut)}
+                        >
+                          <CheckCheck className="mr-2 h-4 w-4" />
+                          Available
+                        </Button>
+                      ) : (
+                        <span className="text-lg font-semibold text-red-600">
+                          Sold out
+                        </span>
+                      )}
+                    </div>
+                    <img
+                      src={prd.Image}
+                      alt="Product Image"
+                      className="mb-2 h-32 w-full rounded-md object-contain"
+                    />
+                    <div>
+                      <h2 className="flex h-9 items-center justify-center overflow-y-hidden text-ellipsis text-sm font-semibold">
+                        {prd.ProductName}
+                      </h2>
+                      <p className="text-gray-700">{formatted(prd.Price)}</p>
+                    </div>
+                  </div>
+                ) : (
+                  <div
+                    key={prd.ProductId}
+                    className="relative flex cursor-pointer flex-col justify-between rounded-lg border p-3 px-1 text-center shadow-md transition duration-150 ease-in-out hover:bg-gray-100 active:scale-95 active:shadow-lg"
+                    onClick={() => {if(rightClick != prd.ProductId){clickPrd(prd)}}}
+                    onContextMenu={(e) => {
+                      e.preventDefault(); // Ngăn chặn menu ngữ cảnh mặc định
+                      setCheckRightClick(prd.ProductId);
+                    }}
+                  >
+                    {rightClick == prd.ProductId && (
+                      <div className="absolute inset-0 flex items-center justify-center rounded-lg border bg-gray-500 bg-opacity-70">
+                        <Button
+                          size="sm"
+                          variant="secondary"
+                          onClick={() => updateSoldOut(prd.ProductId,prd.IsSoldOut)}
+                          className="bg-red-500 text-white hover:bg-red-600"
+                        >
+                          <Ban className="mr-2 h-4 w-4" />
+                          Sold out
+                        </Button>
+                      </div>
+                    )}
+                    <img
+                      src={prd.Image}
+                      alt="Product Image"
+                      className="mb-2 h-32 w-full rounded-md object-contain"
+                    />
+                    <div>
+                      <h2 className="flex h-9 items-center justify-center overflow-y-hidden text-ellipsis text-sm font-semibold">
+                        {prd.ProductName}
+                      </h2>
+                      <p className="text-gray-700">{formatted(prd.Price)}</p>
+                    </div>
+                  </div>
+                );})}
+            </div>
+          </ScrollArea>
+        )}
+      </div>
+    );
 }
